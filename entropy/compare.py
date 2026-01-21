eda4f90d369d6c4b9d5c4232823c51ec451d4663
sjgerstner <sgerstner@cis.uni-muenchen.de>
2026-01-21 14:20:59 +0100

    full ablations: hacky debug and results

--- entropy/compare.py
+++ entropy/compare.py
@@ -18,11 +18,21 @@ def unflattened_data(data_path, metric, neuron_subset_name, intervention_type='z
         f'{data_path}/{neuron_subset_name}/{intervention_type}_None/{metric}.pt',
         weights_only=True
     )#sample pos
+    if baseline.shape[0]!=ablated.shape[0]:
+        #for some reason an earlier version of dolma-small had 45734 rows
+        # while the new version has 45736.
+        # But (I'm pretty sure) the rows are in the same order,
+        # so it should be possible to just remove the last two from the longer version
+        min_shape = min(baseline.shape[0], ablated.shape[0])
+        baseline = baseline[:min_shape]
+        ablated = ablated[:min_shape]
     #print('computing difference...')
     if metric=='scale':
-        diff = baseline / ablated
-    else:
-        diff = baseline - ablated
+        baseline = torch.log(baseline)
+        ablated = torch.log(ablated)
+        #diff = baseline / ablated
+    #else:
+    diff = baseline - ablated
     return diff
 
 def compute_data(data_path, metric, neuron_subset_name, intervention_type='zero_ablation'):
@@ -77,6 +87,7 @@ def compare(args, metric, neuron_subset_names, intervention_type='zero_ablation'
         os.mkdir(experiment_dir)
     if args.log:
         kwargs["log"]=True
+    metric = "log_scale" if metric=='scale' else metric
     aligned_histograms(
         list_data,
         subtitles=subtitles,
