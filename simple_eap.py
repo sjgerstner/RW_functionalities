b4c65bcac48b831c184022157b5bcf9de4df8b8c
sjgerstner <sgerstner@cis.uni-muenchen.de>
2026-04-24 12:05:33 +0200

    update simple_eap and eap code

--- simple_eap.py
+++ simple_eap.py
@@ -17,11 +17,6 @@ from eap.utils import get_logit_positions#, tokenize_plus
 
 from examples import create_args, find_neurons, list_ablation_hooks
 
-#TODO everywhere this appears:
-# - pt instead of json
-# - more fine-grained naming (positional vs not, ...)
-GRAPH_FILE = "../RW_functionalities_results/full_graph.json"
-
 def collate_fn(batch):
     sequences = [item["sequences"] for item in batch]
     labels = [item["labels"] for item in batch]
@@ -45,19 +40,22 @@ def loss(logits:torch.Tensor, clean_logits:torch.Tensor, input_lengths:torch.Ten
 parser = ArgumentParser()
 parser.add_argument("--device", default="cuda:0")
 parser.add_argument("--n_edges", default=32, type=int)
+parser.add_argument("--positional", action='store_true')
+parser.add_argument("--graph_method", choices=["topn", "threshold", "greedy"], default="greedy")
+parser.add_argument("--force_recompute", action='store_true')
+parser.add_argument("--test", action='store_true')
 args = parser.parse_args()
 
-if exists(GRAPH_FILE):
-    graph = Graph.from_json(GRAPH_FILE)
-else:
-    #TODO:
-    # Modify Graph to allow for finding position-sensitive circuits.
-    # Don't change the structure of the graph, just
-    # the shape of graph.scores
-    # and the methods.
+POSITIONAL = args.positional or args.test
+GRAPH_FILE = f"../RW_functionalities_results/full{"_positional" if POSITIONAL else ""}_graph.pt"
 
+if exists(GRAPH_FILE) and not args.force_recompute:
+    print("loading previously computed graph")
+    graph = Graph.from_pt(GRAPH_FILE)
+else:
     #model and graph
-    model = HookedTransformer.from_pretrained_no_processing(#we don't need processing because EAP is implementation invariant
+    #we don't need processing because EAP is implementation invariant
+    model = HookedTransformer.from_pretrained_no_processing(
         'allenai/OLMo-7B-0424-hf',
         #refactor_glu=True,
         device=args.device,
@@ -77,7 +75,7 @@ else:
 
     my_metric=mic_score #TODO also try with entropy and loss (and put minus in front)
 
-    args = create_args(#NOTE that this function is hard-coding the model name
+    my_args = create_args(#NOTE: this function is hard-coding the model name
         neuron_subset_name="weakening",
         intervention_type="zero_ablation",
         metric="entropy",
@@ -86,9 +84,12 @@ else:
         gate='-',
         post='+',
     )
-    neuron_list = find_neurons(args)
-    corruption_hooks = list_ablation_hooks(args, neuron_list)
+    neuron_list = find_neurons(my_args)
+    corruption_hooks = list_ablation_hooks(my_args, neuron_list)
 
+    print("computing scores...")
+    print("metric:", my_metric)
+    print("lower is better:", my_metric is not mic_score)
     #if is_fancy_graph:
     #attribute
     #running this modifies graph.scores:
@@ -99,33 +100,68 @@ else:
         metric=my_metric,
         method='EAP',
         corruption_hooks=corruption_hooks,
-        # keep_pos_dim=True,
+        keep_pos_dims=POSITIONAL,
+        lower_is_better= my_metric is not mic_score,
     )
 
-    graph.to_json(GRAPH_FILE)
+    graph.to_pt(GRAPH_FILE)
 
-#circuit finding
-graph.apply_greedy(
-    n_edges=args.n_edges,
-    absolute=False,
-    prune=False,
-    #prune_parentless=False,
-)
+if POSITIONAL:
+    print(graph.positional_scores[:,-1,-1])
+else:
+    print(graph.scores[-1,-1])
 
-#image
-graph.to_image("../RW_functionalities_results/graph.png")
+print("circuit finding...")
+if args.test:
+    graph.apply_threshold(
+        0.1,
+        absolute=False,
+        prune=False,
+        positional=True,
+    )
+    graph.to_image(
+        f"../RW_functionalities_results/threshold_graph.png",
+        positional=True,
+    )
+    graph.prune(prune_parentless=False, positional=True)
+elif args.graph_method=="greedy":
+    graph.apply_greedy(
+        n_edges=args.n_edges,
+        absolute=False,
+        prune=False,
+        positional=POSITIONAL,
+    )
+elif args.graph_method=="topn":
+    graph.apply_topn(
+        n=args.n_edges,
+        absolute=False,
+        prune=True,
+        prune_parentless=False,
+        positional=POSITIONAL,
+    )
+else:
+    raise NotImplementedError
 
-#TODO evaluation: how similar are the results if we just ablate the relevant edges?
-# and if we just ablate the relevant weakening neurons?
+print("making image...")
+if args.test:
+    graph.to_image(
+        f"../RW_functionalities_results/threshold_graph_pruned.png",
+        positional=True,
+    )
+else:
+    graph.to_image(
+        f"../RW_functionalities_results/{"positional_" if POSITIONAL else ""}graph.png",
+        positional=POSITIONAL,
+    )
+
+#TODO evaluation:
+# - does patching corrupted activations into any of the edges reduce mic score?
+#   - or any combination of the edges?
+# - how about all the final edges together?
+# - how about all the initial edges together?
+# - how about patching clean activations into the corrupted run: does it restore mic score?
+
+#baseline = evaluate_baseline(model, dataloader, metrics=my_metric) -> we already did that
 #results = evaluate_graph(model, graph, dataloader, metrics=my_metric)
 
-# else:#minimalist alternative without fancy graph
-#     scores = attribute.get_scores_eap(
-#         model,
-#         graph,
-#         metric=my_metric,
-#         dataloader=dataloader,
-#         keep_pos_dims=True,
-#         corruption_hooks=corruption_hooks,
-#     )
-#     #print(torch.topk(scores, k=16))
+# TODO which are the relevant weakening neurons, and what if we ablate just them?
