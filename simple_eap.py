ff00fe7eaf71183767e0aaa1896cdb55e1ca4c7e
sjgerstner <sgerstner@cis.uni-muenchen.de>
2026-05-11 16:52:59 +0200

    update / debug eap code

--- simple_eap.py
+++ simple_eap.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from os.path import exists
+import os
 
 import torch
 from torch.utils.data import (
@@ -47,20 +47,27 @@ parser.add_argument("--force_recompute", action='store_true')
 parser.add_argument("--test", action='store_true')
 args = parser.parse_args()
 
-POSITIONAL = args.positional or args.test
+POSITIONAL = args.positional# or args.test
 SUBNODES = args.subnodes or args.test
-GRAPH_FILE = f"../RW_functionalities_results/full{"_positional" if POSITIONAL else ""}_graph{"_with_subnodes" if SUBNODES else ""}.pt"
+GRAPH_FILE = f"../RW_functionalities_results/full{"_positional" if POSITIONAL else ""}_graph{"_with_subnodes" if SUBNODES else ""}"
 
-if exists(GRAPH_FILE) and not args.force_recompute:
+if os.path.exists(GRAPH_FILE+'.pt') and not args.force_recompute:
     print("loading previously computed graph")
-    graph = Graph.from_pt(GRAPH_FILE)
+    graph = Graph.from_pt(GRAPH_FILE+'.pt')
 else:
+    if args.device.startswith("cuda") and not torch.cuda.is_available():
+        print("Specified cuda, but cuda is not available. Continuing on cpu...")
+        DEVICE = "cpu"
+    else:
+        DEVICE = args.device
+    if DEVICE.startswith('cuda') and SUBNODES:
+        os.environ["PYTORCH_CUDA_ALLOC_CONF"]="expandable_segments:True"
     #model and graph
-    #we don't need processing because EAP is implementation invariant
-    model = HookedTransformer.from_pretrained_no_processing(
+
+    model = HookedTransformer.from_pretrained(#weight processing is needed for consistent definition of weakening neurons
         'allenai/OLMo-7B-0424-hf',
-        #refactor_glu=True,
-        device=args.device,
+        #refactor_glu=True,#we don't need this because EAP is implementation invariant
+        device=DEVICE,
     )
     model.cfg.use_attn_result = True
     model.cfg.use_split_qkv_input = True
@@ -109,55 +116,62 @@ else:
         lower_is_better= my_metric is not mic_score,
     )
 
-    graph.to_pt(GRAPH_FILE)
+    if SUBNODES:
+        subnodes_df = graph.subnodes_to_pandas('../RW_functionalities_results/subnodes.csv')
+        subnodes_df = subnodes_df.sort_values("score", ascending=False)
+        print(subnodes_df.head(40))
+
+    graph.to_pt(GRAPH_FILE+'.pt')
 
 # if POSITIONAL:
 #     print(graph.positional_scores[:,-1,-1])
 # else:
 #     print(graph.scores[-1,-1])
 
-print("circuit finding...")
-if args.test:
-    graph.apply_threshold(
-        0.1,
-        absolute=False,
-        prune=False,
-        positional=True,
-    )
-    graph.to_image(
-        f"../RW_functionalities_results/threshold_graph.png",
-        positional=True,
-    )
-    graph.prune(prune_parentless=False, positional=True)
-elif args.graph_method=="greedy":
-    graph.apply_greedy(
-        n_edges=args.n_edges,
-        absolute=False,
-        prune=False,
-        positional=POSITIONAL,
-    )
-elif args.graph_method=="topn":
-    graph.apply_topn(
-        n=args.n_edges,
-        absolute=False,
-        prune=True,
-        prune_parentless=False,
-        positional=POSITIONAL,
-    )
-else:
-    raise NotImplementedError
+if not SUBNODES:
+    print("circuit finding...")
+    if args.test:
+        graph.apply_threshold(
+            0.1,
+            absolute=False,
+            prune=False,
+            positional=POSITIONAL,
+            level='subnode' if SUBNODES else 'edge',
+        )
+        graph.to_image(
+            GRAPH_FILE+'_threshold.png',
+            positional=POSITIONAL,
+        )
+        graph.prune(prune_parentless=False, positional=POSITIONAL)
+    elif args.graph_method=="greedy":
+        graph.apply_greedy(
+            n_edges=args.n_edges,
+            absolute=False,
+            prune=False,
+            positional=POSITIONAL,
+        )
+    elif args.graph_method=="topn":
+        graph.apply_topn(
+            n=args.n_edges,
+            absolute=False,
+            prune=True,
+            prune_parentless=False,
+            positional=POSITIONAL,
+        )
+    else:
+        raise NotImplementedError
 
-print("making image...")
-if args.test:
-    graph.to_image(
-        f"../RW_functionalities_results/threshold_graph_pruned.png",
-        positional=True,
-    )
-else:
-    graph.to_image(
-        f"../RW_functionalities_results/{"positional_" if POSITIONAL else ""}graph.png",
-        positional=POSITIONAL,
-    )
+    print("making image...")
+    if args.test:
+        graph.to_image(
+            GRAPH_FILE+'_threshold_pruned.png',
+            positional=POSITIONAL,
+        )
+    else:
+        graph.to_image(
+            GRAPH_FILE+f'_{args.graph_method}.png',
+            positional=POSITIONAL,
+        )
 
 #TODO evaluation:
 # - does patching corrupted activations into any of the edges reduce mic score?
