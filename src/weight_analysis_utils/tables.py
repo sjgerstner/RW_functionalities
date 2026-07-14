0f2bc785f25ca9a962a3edd8385b68338e3b9a8c
sjgerstner <sgerstner@cis.uni-muenchen.de>
2026-07-14 15:13:11 +0200

    complete quartile function

--- src/weight_analysis_utils/tables.py
+++ src/weight_analysis_utils/tables.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from pandas.io.formats.style import Styler
 
@@ -17,15 +18,19 @@ def make_table(layerwise_counts):
     return styler
 
 def quartile_table(
-    list_data:list[np.ndarray], subtitles:list[str], savefile:str,
-    suptitle:str|None=None, xlabel:str='', ylabel:str='number of model predictions',
-    #ncols:int=1, n_bins:int | Sequence[float] | str | None=None, weighted=False,
-    **kwargs
+    list_data:list[np.ndarray], subtitles:list[str],
+    # savefile:str,
+    # suptitle:str|None=None, xlabel:str='', ylabel:str='number of model predictions',
+    # **kwargs
 ):
+    quantiles = [np.quantile(data, q=[.25, .5, .75]) for data in list_data]
     df = pd.DataFrame.from_dict(
         {
             "name": subtitles,
-            #TODO
+            "mean": [data.mean() for data in list_data],
+            "quartile1": [quantile[0] for quantile in quantiles],
+            "median": [quantile[1] for quantile in quantiles],
+            "quartile3": [quantile[2] for quantile in quantiles],
         },
         orient='columns',
     )
